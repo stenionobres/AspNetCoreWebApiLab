3dfdc7d023f2a1f1fc54568283360382d5fd885c
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-02-24 19:02:55 -0300

    Adds use example of resources, verbs and status codes

---
+++ AspNetCoreWebApiLab.Api/Controllers/Experiments/ResourceController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using AspNetCoreWebApiLab.Api.Models.Experiments;
+
+namespace AspNetCoreWebApiLab.Api.Controllers.Experiments
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourceController : ControllerBase
+    {
+        private dynamic _resource = new { Id = 1, Description = "First resource" };
+
+        [HttpGet]
+        public ActionResult GetResource(int resourceId)
+        {
+            try
+            {
+                if (resourceId != 1) return NotFound("Resource not found");
+
+                return Ok(_resource);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult PostResource(ResourceModel resourceModel)
+        {
+            try
+            {
+                return Created($"/api/resources/{resourceModel.Id}", resourceModel);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        [HttpPut]
+        public ActionResult PutResource(ResourceModel resourceModel)
+        {
+            try
+            {
+                if (resourceModel.Id != 1) return NotFound("Resource not found");
+
+                return Ok(resourceModel);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        [HttpPatch]
+        public ActionResult PatchResource(ResourceModel resourceModel)
+        {
+            try
+            {
+                if (resourceModel.Id != 1) return NotFound("Resource not found");
+
+                return Ok(resourceModel);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteResource(int resourceId)
+        {
+            try
+            {
+                if (resourceId != 1) return NotFound("Resource not found");
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+    }
+}
---
+++ AspNetCoreWebApiLab.Api/Models/Experiments/ResourceModel.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreWebApiLab.Api.Models.Experiments
+{
+    public class ResourceModel
+    {
+        [Required]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(100, MinimumLength = 10)]
+        public string Description { get; set; }
+    }
+}
