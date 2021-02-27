6abd2db7616c61a6885e8a5597b36be8f4eaaf5a
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-02-27 10:31:27 -0300

    Adds get action on Roles api

---
+++ AspNetCoreWebApiLab.Api/Controllers/V1/RolesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using AspNetCoreWebApiLab.Api.Models.V1;
+
+namespace AspNetCoreWebApiLab.Api.Controllers.V1
+{
+    [ApiController]
+    [Route("api/roles")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    public class RolesController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(RoleModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult GetRoles(int roleId)
+        {
+            try
+            {
+                if (roleId != 1) return NotFound("Role not found");
+
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+    }
+}
---
+++ AspNetCoreWebApiLab.Api/Models/V1/RoleModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreWebApiLab.Api.Models.V1
+{
+    public class RoleModel
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public int Description { get; set; }
+    }
+}
