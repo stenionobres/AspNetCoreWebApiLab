76a2b152e104327c4f4f7ceb902befa06f0c63c1
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-02-27 17:04:40 -0300

    Adds delete action on UserRoles api

--- AspNetCoreWebApiLab.Api/Controllers/V1/UserRolesController.cs
+++ AspNetCoreWebApiLab.Api/Controllers/V1/UserRolesController.cs
@@ -64,5 +64,24 @@ namespace AspNetCoreWebApiLab.Api.Controllers.V1
                 return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
             }
         }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult DeleteUserRoles(int id, int roleId)
+        {
+            try
+            {
+                if (id != 1) return NotFound("User not found");
+
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
     }
 }
