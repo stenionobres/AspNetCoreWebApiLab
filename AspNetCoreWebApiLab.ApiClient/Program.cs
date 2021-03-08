6149a2c8d0ad8dd7882d57741fcc290d56dc9f47
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-03-08 16:59:56 -0300

    Adds AspNetCoreWebApiLab.ApiClient project with RestSharp

---
+++ AspNetCoreWebApiLab.ApiClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using RestSharp;
+using System;
+
+namespace AspNetCoreWebApiLab.ApiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var client = new RestClient("https://localhost:44325/api/");
+            var request = new RestRequest("roles/7", Method.GET);
+
+            var response = client.Execute(request);
+        }
+    }
+}
