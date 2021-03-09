293607a3d122306be14d392d61af4656eb61b534
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-03-09 17:23:04 -0300

    Adds get request on UsersService

--- AspNetCoreWebApiLab.ApiClient/Services/UsersService.cs
+++ AspNetCoreWebApiLab.ApiClient/Services/UsersService.cs
@@ -1,10 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using RestSharp;
+using AspNetCoreWebApiLab.ApiClient.DTOs;
 
 namespace AspNetCoreWebApiLab.ApiClient.Services
 {
     public class UsersService
     {
+        private RestClient _restClient;
+
+        private const string ResourceName = "users";
+
+        public UsersService(string baseUrl)
+        {
+            _restClient = new RestClient(baseUrl);
+        }
+
+        public User GetUser(int id)
+        {
+            var request = new RestRequest($"{ResourceName}/{id}", Method.GET);
+            var response = _restClient.Execute<User>(request);
+
+            if (response.IsSuccessful)
+            {
+                return response.Data;
+            }
+
+            throw new ApplicationException($"{Convert.ToInt32(response.StatusCode)}: {response.Content}");
+        }
     }
 }
