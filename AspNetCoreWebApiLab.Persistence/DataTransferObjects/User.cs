ffabd533337cce7234b03ae159f0b6d56ebef448
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-03-12 15:44:01 -0300

    Adds User DataTransferObject to Persistence project

---
+++ AspNetCoreWebApiLab.Persistence/DataTransferObjects/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreWebApiLab.Persistence.DataTransferObjects
+{
+    public class User : IdentityUser<int>
+    {
+        [Required]
+        [StringLength(30)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Occupation { get; set; }
+    }
+}
