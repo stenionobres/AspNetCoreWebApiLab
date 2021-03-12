8dabbb5f8e64b4ccfba37d31829f756185cf2328
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-03-12 16:43:53 -0300

    Adds AspNetCoreWebApiLabDbContext
    To connect with database to recover and persist data

---
+++ AspNetCoreWebApiLab.Persistence/EntityFrameworkContexts/AspNetCoreWebApiLabDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using AspNetCoreWebApiLab.Persistence.DataTransferObjects;
+
+namespace AspNetCoreWebApiLab.Persistence.EntityFrameworkContexts
+{
+    public class AspNetCoreWebApiLabDbContext : IdentityDbContext<User, IdentityRole<int>, int>
+    {
+        private const string ConnectionString = @"Server=192.168.0.13,22331;Database=AspNetCoreWebApiLab;User ID=sa;Password=sqlserver.252707;
+                                                  Encrypt=False;Trusted_Connection=False;Connection Timeout=3000;";
+
+        public static readonly ILoggerFactory LoggerFactoryToConsole = LoggerFactory.Create(builder => builder.AddConsole());
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured) return;
+
+            optionsBuilder.UseSqlServer(ConnectionString);
+            optionsBuilder.UseLoggerFactory(LoggerFactoryToConsole);
+        }
+    }
+}
