7e84361cd8ed1bbb7fd084a26ff35ce9c20a879b
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-05-13 08:45:19 -0300

    Adds Roles and Users initial controllers of API 3.0

---
+++ AspNetCoreWebApiLab.Api/Controllers/V3/RolesController.cs
@@ -0,0 +1,224 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using AspNetCoreWebApiLab.Api.Models.V1;
+using AspNetCoreWebApiLab.Api.Services;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AspNetCoreWebApiLab.Api.Controllers.V3
+{
+    [ApiController]
+    [Authorize]
+    [ApiVersion("3.0")]
+    [Route("api/v{version:apiVersion}/roles")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [ApiExplorerSettings(GroupName = "IdentityAPI-V3.0")]
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleService _roleService;
+        private readonly RoleClaimService _roleClaimService;
+
+        public RolesController(RoleService roleService, RoleClaimService roleClaimService)
+        {
+            _roleService = roleService;
+            _roleClaimService = roleClaimService;
+        }
+
+        [HttpGet("{roleId}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(RoleModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> GetRoles(int roleId)
+        {
+            try
+            {
+                var role = await _roleService.GetAsync(roleId);
+
+                if (role == null) return NotFound("Role not found");
+
+                return Ok(role);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(RoleModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> PostRoles(RoleModel role)
+        {
+            try
+            {
+                await _roleService.SaveAsync(role);
+
+                return Created($"/api/roles/{role.Id}", role);
+            }
+            catch (System.ApplicationException ex)
+            {
+                return StatusCode(StatusCodes.Status422UnprocessableEntity, ex.Message);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(RoleModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> PutRoles(RoleModel role)
+        {
+            try
+            {
+                var roleSaved = await _roleService.GetAsync(role.Id);
+
+                if (roleSaved == null) return NotFound("Role not found");
+
+                await _roleService.UpdateAsync(roleSaved, role);
+
+                return Ok(role);
+            }
+            catch (System.ApplicationException ex)
+            {
+                return StatusCode(StatusCodes.Status422UnprocessableEntity, ex.Message);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        [HttpDelete("{roleId}")]
+        [Authorize(Roles = "Admin, CanDeleteRoles")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> DeleteRoles(int roleId)
+        {
+            try
+            {
+                var role = await _roleService.GetAsync(roleId);
+
+                if (role == null) return NotFound("Role not found");
+
+                await _roleService.DeleteAsync(role);
+
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        [HttpOptions]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult OptionsRoles()
+        {
+            Response.Headers.Add("Allow", "GET,POST,PUT,DELETE");
+            return Ok();
+        }
+
+        /// <summary>
+        /// Associates a claim with a role. If claim doesn't exists it's created with new id.
+        /// </summary>
+        /// <param name="roleId"></param>
+        /// <param name="claim"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("{roleId}/claims")]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ClaimModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> PostRoleClaims(int roleId, ClaimModel claim)
+        {
+            try
+            {
+                var role = await _roleService.GetAsync(roleId);
+
+                if (role == null) return NotFound("Role not found");
+
+                await _roleClaimService.AssociateAsync(role.Id, claim);
+
+                return Created($"/api/roles/{roleId}/claims", claim);
+            }
+            catch (System.ApplicationException ex)
+            {
+                return StatusCode(StatusCodes.Status422UnprocessableEntity, ex.Message);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        /// <summary>
+        /// Get claims associated with role.
+        /// </summary>
+        /// <param name="roleId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{roleId}/claims")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<ClaimModel>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> GetRoleClaims(int roleId)
+        {
+            try
+            {
+                var role = await _roleService.GetAsync(roleId);
+
+                if (role == null) return NotFound("Role not found");
+
+                var claims = await _roleClaimService.GetClaimsAsyncBy(role);
+
+                return Ok(claims);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        /// <summary>
+        /// Removes claim associated with role.
+        /// </summary>
+        /// <param name="roleId"></param>
+        /// <param name="claim"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("{roleId}/claims")]
+        [Authorize(Roles = "Admin, CanDeleteRoles")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> DeleteRoleClaims(int roleId, ClaimModel claim)
+        {
+            try
+            {
+                await _roleClaimService.RemoveAssociationAsync(roleId, claim);
+
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+    }
+}
---
+++ AspNetCoreWebApiLab.Api/Controllers/V3/UsersController.cs
@@ -0,0 +1,381 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreWebApiLab.Api.Models.V1;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.JsonPatch;
+using AspNetCoreWebApiLab.Api.Services;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AspNetCoreWebApiLab.Api.Controllers.V3
+{
+    [ApiController]
+    [Authorize]
+    [ApiVersion("3.0")]
+    [Route("api/v{version:apiVersion}/users")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [ApiExplorerSettings(GroupName = "IdentityAPI-V3.0")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService _userService;
+        private readonly UserRoleService _userRoleService;
+        private readonly UserClaimService _userClaimService;
+
+        public UsersController(UserService userService, UserRoleService userRoleService, UserClaimService userClaimService)
+        {
+            _userService = userService;
+            _userRoleService = userRoleService;
+            _userClaimService = userClaimService;
+        }
+
+        [HttpGet("{userId}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> GetUsers(int userId)
+        {
+            try
+            {
+                var user = await _userService.GetAsync(userId);
+
+                if (user == null) return NotFound("User not found");
+
+                return Ok(user);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin, CanManageUsers")]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(UserModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> PostUsers(UserPostModel user)
+        {
+            try
+            {
+                var userCreated = await _userService.SaveAsync(user);
+
+                return Created($"/api/users/{user.Id}", userCreated);
+            }
+            catch (System.ApplicationException ex)
+            {
+                return StatusCode(StatusCodes.Status422UnprocessableEntity, ex.Message);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "Admin, CanManageUsers")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> PutUsers(UserModel user)
+        {
+            try
+            {
+                var userSaved = await _userService.GetAsync(user.Id);
+
+                if (userSaved == null) return NotFound("User not found");
+
+                await _userService.UpdateAsync(userSaved.Id, user);
+
+                return Ok(user);
+            }
+            catch (System.ApplicationException ex)
+            {
+                return StatusCode(StatusCodes.Status422UnprocessableEntity, ex.Message);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        [HttpPatch("{userId}")]
+        [Authorize(Roles = "Admin, CanManageUsers")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> PatchUsers(int userId, JsonPatchDocument<UserModel> userModelPatchDocument)
+        {
+            try
+            {
+                var user = await _userService.GetAsync(userId);
+
+                if (user == null) return NotFound("User not found");
+
+                userModelPatchDocument.ApplyTo(user);
+
+                await _userService.UpdateAsync(userId, user);
+
+                return Ok(user);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        [HttpDelete("{userId}")]
+        [Authorize(Roles = "Admin, CanManageUsers")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> DeleteUsers(int userId)
+        {
+            try
+            {
+                var user = await _userService.GetAsync(userId);
+
+                if (user == null) return NotFound("User not found");
+
+                await _userService.DeleteAsync(user);
+
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        [HttpOptions]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult OptionsUsers()
+        {
+            Response.Headers.Add("Allow", "GET,POST,PUT,PATCH,DELETE");
+            return Ok();
+        }
+
+        /// <summary>
+        /// Associates an user with a role. If role doesn't exists it's created with new id.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("{userId}/roles")]
+        [Authorize(Roles = "Admin, CanManageUsers")]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(RoleModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> PostUserRoles(int userId, RoleModel role)
+        {
+            try
+            {
+                var user = await _userService.GetAsync(userId);
+
+                if (user == null) return NotFound("User not found");
+
+                await _userRoleService.AssociateAsync(userId, role);
+
+                return Created($"/api/users/{userId}/roles", role);
+            }
+            catch (System.ApplicationException ex)
+            {
+                return StatusCode(StatusCodes.Status422UnprocessableEntity, ex.Message);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        /// <summary>
+        /// Get roles associated with user.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{userId}/roles")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<RoleModel>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> GetUserRoles(int userId)
+        {
+            try
+            {
+                var user = await _userService.GetAsync(userId);
+
+                if (user == null) return NotFound("User not found");
+
+                var roles = await _userRoleService.GetRolesAsyncBy(user);
+
+                return Ok(roles);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        /// <summary>
+        /// Removes role associated with user.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="roleName"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("{userId}/roles/{roleName}")]
+        [Authorize(Roles = "Admin, CanManageUsers")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> DeleteUserRoles(int userId, string roleName)
+        {
+            try
+            {
+                await _userRoleService.RemoveAssociationAsync(userId, roleName);
+
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        /// <summary>
+        /// Associates an user with a claim. If claim doesn't exists it's created with new id.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="claim"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("{userId}/claims")]
+        [Authorize(Roles = "Admin, CanManageUsers")]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ClaimModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> PostUserClaims(int userId, ClaimModel claim)
+        {
+            try
+            {
+                var user = await _userService.GetAsync(userId);
+
+                if (user == null) return NotFound("User not found");
+
+                await _userClaimService.AssociateAsync(userId, claim);
+
+                return Created($"/api/users/{userId}/claims", claim);
+            }
+            catch (System.ApplicationException ex)
+            {
+                return StatusCode(StatusCodes.Status422UnprocessableEntity, ex.Message);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        /// <summary>
+        /// Get claims associated with user.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{userId}/claims")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<ClaimModel>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> GetUserClaims(int userId)
+        {
+            try
+            {
+                var user = await _userService.GetAsync(userId);
+
+                if (user == null) return NotFound("User not found");
+
+                var claims = await _userClaimService.GetClaimsAsyncBy(user);
+
+                return Ok(claims);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        /// <summary>
+        /// Removes claims associated with user.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="claim"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("{userId}/claims")]
+        [Authorize(Roles = "Admin, CanManageUsers")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> DeleteUserClaims(int userId, ClaimModel claim)
+        {
+            try
+            {
+                await _userClaimService.RemoveAssociationAsync(userId, claim);
+
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+
+        /// <summary>
+        /// Gets JWT Token for authenticate on API
+        /// </summary>
+        /// <param name="signInModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("signin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> SignIn(SignInModel signInModel)
+        {
+            try
+            {
+                var jwtToken = await _userService.SignInAsync(signInModel);
+
+                if (string.IsNullOrEmpty(jwtToken)) return NotFound("User not found");
+
+                return Ok(new { token = jwtToken });
+            }
+            catch (System.ApplicationException ex)
+            {
+                return StatusCode(StatusCodes.Status422UnprocessableEntity, ex.Message);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error has occurred");
+            }
+        }
+    }
+}
