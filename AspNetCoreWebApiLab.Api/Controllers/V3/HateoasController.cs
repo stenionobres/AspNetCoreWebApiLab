2cf0c54745f52023949369f367ad708d2a86476c
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-05-25 10:04:11 -0300

    Adds Hateoas controller with root links API

---
+++ AspNetCoreWebApiLab.Api/Controllers/V3/HateoasController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using AspNetCoreWebApiLab.Api.Models.V3;
+
+namespace AspNetCoreWebApiLab.Api.Controllers.V3
+{
+    [ApiController]
+    [ApiVersion("3.0")]
+    [Route("api/v{version:apiVersion}")]
+    [ApiExplorerSettings(GroupName = "IdentityAPI-V3.0")]
+    public class HateoasController : ControllerBase
+    {
+        [HttpGet(Name = "ApiEndpoints")]
+        public IActionResult GetApiEndpoints()
+        {
+            var endpoints = new List<Endpoint>();
+            var host = Request.Host;
+            var version = "v3";
+
+            endpoints.Add(new Endpoint($"{host}/api/{version}", "self", "GET"));
+            endpoints.Add(new Endpoint($"{host}/api/{version}/users", "create_user", "POST"));
+            endpoints.Add(new Endpoint($"{host}/api/{version}/users/signin", "signin_user", "POST"));
+
+            return Ok(endpoints);
+        }
+    }
+}
---
+++ AspNetCoreWebApiLab.Api/Models/V3/Endpoint.cs
@@ -0,0 +1,17 @@
+﻿
+namespace AspNetCoreWebApiLab.Api.Models.V3
+{
+    public class Endpoint
+    {
+        public string HRef { get; set; }
+        public string Rel { get; set; }
+        public string Method { get; set; }
+
+        public Endpoint(string href, string rel, string method)
+        {
+            HRef = href;
+            Rel = rel;
+            Method = method;
+        }
+    }
+}
